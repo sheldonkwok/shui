09c66d53c4f61a5fde75074c729b371aa80705e6
Sheldon Kwok <me@sheldonk.com>
2025-11-21 13:49:05 -0800

    refactor: Move css into its own file and test per file css

--- src/components/PlantList.tsx
+++ src/components/PlantList.tsx
@@ -1,19 +1,22 @@
 import { Plant } from "./Plant.tsx";
 import { getPlants } from "../actions/plants.ts";
+import styles from "./PlantList.module.css";
 
 export async function PlantList() {
   const plants = await getPlants();
 
   return (
-    <div className="plant-list">
+    <div className={styles["container"]}>
       {plants.length > 0 ? (
-        <ul>
+        <ul className={styles["list"]}>
           {plants.map((plant) => (
             <Plant key={plant.id} plant={plant} />
           ))}
         </ul>
       ) : (
-        <p className="no-plants">No plants yet. Add your first plant below!</p>
+        <p className={styles["noPlants"]}>
+          No plants yet. Add your first plant below!
+        </p>
       )}
     </div>
   );
--- src/env.d.ts
+++ src/env.d.ts
@@ -13,3 +13,13 @@ declare namespace App {
     session?: AuthenticatedSession;
   }
 }
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
