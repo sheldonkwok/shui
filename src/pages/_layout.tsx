dc8b8914e3a6443a03e22ebf94bb5622ba8d6797
Sheldon Kwok <me@sheldonk.com>
2026-01-18 12:35:40 -0800

    feat: Add new shui favicon

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -10,7 +10,7 @@ export default async function Layout({ children }: LayoutProps) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/shui.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Shui App</title>
       </head>
