6bf6bf10368a8165e006ddceb52f0decd5e8f911
Sheldon Kwok <me@sheldonk.com>
2026-02-14 00:16:36 -0800

    feat: Add CVA

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,8 +1,14 @@
+import { cva } from "class-variance-authority";
+
+const menubar = cva("flex items-center gap-2 pb-2");
+const logo = cva("w-[30px] h-[30px]");
+const title = cva("text-2xl font-bold");
+
 export function Menubar() {
   return (
-    <div className="flex items-center gap-2 pb-2">
-      <img src="/shui.png" alt="Shui" className="w-[30px] h-[30px]" />
-      <span className="text-2xl font-bold">Shui</span>
+    <div className={menubar()}>
+      <img src="/shui.png" alt="Shui" className={logo()} />
+      <span className={title()}>Shui</span>
     </div>
   );
 }
