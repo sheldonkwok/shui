ed5acd82a2f3e57d2e1b2ca949cf8f23ceca4623
Sheldon Kwok <me@sheldonk.com>
2026-02-13 23:20:22 -0800

    refactor: Try tailwind for menubar

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,27 +1,8 @@
-import { css } from "../../styled-system/css";
-
-const menubarStyles = css({
-  display: "flex",
-  alignItems: "center",
-  gap: "8px",
-  paddingBottom: "8px",
-});
-
-const logoStyles = css({
-  width: "40px",
-  height: "40px",
-});
-
-const titleStyles = css({
-  fontSize: "24px",
-  fontWeight: "bold",
-});
-
 export function Menubar() {
   return (
-    <div className={menubarStyles}>
-      <img src="/shui.png" alt="Shui" className={logoStyles} />
-      <span className={titleStyles}>Shui</span>
+    <div className="flex items-center gap-2 pb-2">
+      <img src="/shui.png" alt="Shui" className="w-[30px] h-[30px]" />
+      <span className="text-2xl font-bold">Shui</span>
     </div>
   );
 }
